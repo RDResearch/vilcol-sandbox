79db9d57c2ce2f78f42aa6137710859cca689eaa
Lara PC <kevin@rdresearch.co.uk>
2022-09-14 16:35:02 +0100

    PDF merge working

--- admin/vendor/composer/autoload_psr4.php
+++ admin/vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'setasign\\Fpdi\\' => array($vendorDir . '/setasign/fpdi/src'),
+    'iio\\libmergepdf\\' => array($vendorDir . '/iio/libmergepdf/src'),
 );
