e56832c45b5b4964cad799496ba0b5fc029f472f
MPSTR <74561948+MPSTR24@users.noreply.github.com>
2022-09-30 12:18:11 +0100

    Update link

--- admin/jobs.php
+++ admin/jobs.php
@@ -7761,7 +7761,7 @@ function mass_print_letters($ticked_jobs, $upload_app=false)
 //
 	print("
 	<span>Mass printer - merged $count letters into 1 PDF</span>
-	<form target=\"_blank\" action=\"https://vilcoldbl.com/admin/$file_name\">
+	<form target=\"_blank\" action=\"https://vilcolbl.com//admin/$file_name\">
 		<input type=\"submit\" value=\"Click to open\" />
 	</form>
 	");
